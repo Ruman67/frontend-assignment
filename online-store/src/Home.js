0f8bd733668ff80e16092d0d4be3ed0b58250ffe
MaharjanRuman <rumanmaharjan969gmail.com>
2023-07-20 21:37:15 +0545

    created home.js

--- online-store/src/Home.js
+++ online-store/src/Home.js
@@ -1,27 +1,24 @@
-
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     // Fetch the list of products from the API endpoint
-    axios
-      .get("https://fakestoreapi.com/products")
+    axios.get('https://fakestoreapi.com/products') // Add quotes around the URL
       .then((response) => setProducts(response.data))
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <div>
-      <h1>Welcome to our Home Page</h1>
+      <h2>Home Page</h2>
       {products.map((product) => (
         <div key={product.id}>
-          <img src={product.image} alt={product.name}
-          style={{ width: '200px', height: '170px' }} />
-          <h3>{product.name}</h3>
-          <p>Price: {product.price}</p>
+          <img src={product.image} alt={product.title} />
+          <h3>{product.title}</h3>
+          <p>Price: ${product.price}</p>
         </div>
       ))}
     </div>
@@ -29,3 +26,9 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+
+
+
