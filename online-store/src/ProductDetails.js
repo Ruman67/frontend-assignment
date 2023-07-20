d19d6fc9d91a88e0c030df3d74819764425b0dc2
MaharjanRuman <rumanmaharjan969gmail.com>
2023-07-20 21:38:32 +0545

    added productdetails

--- online-store/src/ProductDetails.js
+++ online-store/src/ProductDetails.js
@@ -9,14 +9,14 @@ const ProductDetails = () => {
   useEffect(() => {
     // Fetch the product details from the API endpoint
     axios
-      .get(`API_URL/products/${productId}`)
+      .get(`https://fakestoreapi.com/products/${productId}`)
       .then((response) => setProduct(response.data))
       .catch((error) => console.error(error));
   }, [productId]);
 
   return (
     <div>
-      <h2>Product Details Page</h2>
+      <h2>Product Details </h2>
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <p>Price: {product.price}</p>
