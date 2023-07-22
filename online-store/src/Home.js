b1dea3afb5f5579ec13e501db7298cc48acff9e3
Ruman67 <rumanmaharjan888@gmail.com>
2023-07-22 10:54:57 +0545

    added to cart

--- online-store/src/Home.js
+++ online-store/src/Home.js
@@ -1,30 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css'; // Import your CSS file (make sure the file path is correct)
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Home = () => {
+const Home = ({ cart, setCart }) => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     // Fetch the list of products from the API endpoint
-    axios.get('https://fakestoreapi.com/products')
+    axios
+      .get("https://fakestoreapi.com/products")
       .then((response) => setProducts(response.data))
       .catch((error) => console.error(error));
   }, []);
 
+  // Function to add a product to the cart
+  const addToCart = (product) => {
+    setCart((prevCart) => [...prevCart, product]);
+    toast.success(`${product.title} added to cart`, {
+      position: "top-right",
+      autoClose: 2000, // 2 seconds
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
   return (
     <div>
-     
       <div className="product-container">
         {products.map((product) => (
           <div key={product.id} className="product-item">
             <img
               src={product.image}
               alt={product.title}
-              className="product-image" // Apply the CSS class to the image element
+              className="product-image"
             />
             <h3>{product.title}</h3>
             <p>Price: ${product.price}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+            <Link to={`/product/${product.id}`}>
+              <button>View Details</button>
+            </Link>
           </div>
         ))}
       </div>
